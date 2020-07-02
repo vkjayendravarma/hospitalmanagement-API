d5ac2bed04ab89d00ac1b58ea7e3b746c2fb6590
[Keshav Varma] <[keshav@fountane.com]>
2020-07-02 20:13:00 +0530

    new patient intigration

--- src/routes/reception.py
+++ src/routes/reception.py
@@ -1,4 +1,4 @@
-from src import app,db,Config
+from src import app, db, Config
 from flask import request
 from src.models import patientModel
 from flask_mongoengine import mongoengine
@@ -6,52 +6,63 @@ from flask_api import status
 
 from src.routes import shared
 
-@app.route('/reception/patients/new',methods=['POST'])
+
+@app.route('/reception/patients/new', methods=['POST'])
 def newPatient():
+    req = dict(request.get_json())
+   
     try:
-        ssnid = request.form['ssnid']
-        name=request.form['name']
-        age= int(request.form['age'])
-        address= request.form['address']
-        dateOfJoining= request.form['dateOfJoining']
-        roomType= request.form['roomType'] 
-    except KeyError:
-        return {"success": False, "message": "one or more missing fields"}, status.HTTP_400_BAD_REQUEST
-    
+        ssnid = req['ssnid']
+        name = req['name']
+        age = int(req['age'])
+        address = req['address']
+        dateOfJoining = req['dateOfJoining']
+        roomType = req['roomType']
+        city = req['city']
+        state = req['state']
+    except KeyError as e:
+        print(str(e))
+        return "Null"
+
     patientId = str(100000000 + patientModel.Patient.objects().count())
     try:
-        patient = patientModel.Patient(ssnid=ssnid,patientId=patientId, name=name, age =age, address=address, dateOfJoining=dateOfJoining, roomType=roomType).save()
+        patient = patientModel.Patient(ssnid=ssnid, patientId=patientId, name=name, age=age, address=address, dateOfJoining=dateOfJoining, roomType=roomType, city=city, state=state).save()
         patientid = str(patient['patientId'])
-    except mongoengine.errors as i:
-        return i
-    
+    except mongoengine.errors.NotUniqueError as e:
+        return {'success': False, 'message': "SSN ID exists"}
+    except mongoengine.errors.ValidationError as e:
+        return {'success': False, 'message': "Missing field"}
+
     return {
         'success': True,
-        'res': {            
+        'res': {
             'message': 'patient created',
-            'patientId': patientid            
+            'patientId': patientid
         }
     }
-    
+
 # return all patients
-@app.route('/reception/patients',methods=['GET'])
+
+
+@app.route('/reception/patients', methods=['GET'])
 def allPatients():
     data = patientModel.Patient.objects()
     return {
         'success': True,
         'res': data
     }
-    
+
 # GET return patient data
 # POST update patient data
 # PUT change status to Discharged
 # DELETE delete patient
-    
-@app.route('/reception/patients/individual/<patientId>', methods=['GET', 'PUT','POST', 'DELETE'])
+
+
+@app.route('/reception/patients/individual/<patientId>', methods=['GET', 'PUT', 'POST', 'DELETE'])
 def Patient(patientId):
-    
+
     if request.method == 'GET':
-        data = patientModel.Patient.objects(patientId=patientId).first() 
+        data = patientModel.Patient.objects(patientId=patientId).first()
         if(data):
             pharmacyInvoices = shared.getPharmacyInvoices(data.pharmacy)
             data['pharmacy'] = pharmacyInvoices
@@ -61,70 +72,67 @@ def Patient(patientId):
             }
         return {
             'success': False,
-            'message': 'Patient not found'            
+            'message': 'Patient not found'
         }
-        
+
     if request.method == 'POST':
         data = patientModel.Patient.objects(patientId=patientId)
         if(data):
             print(data)
+            req = request.get_json()
+            
+
             try:
-                ssnid = request.form['ssnid']
-                name=request.form['name']
-                age= int(request.form['age'])
-                address= request.form['address']
-                dateOfJoining= request.form['dateOfJoining']
-                roomType= request.form['roomType'] 
+                ssnid = req['ssnid']
+                name = req['name']
+                age = int(req['age'])
+                address = req['address']
+                dateOfJoining = req['dateOfJoining']
+                roomType = req['roomType']
+
             except KeyError:
                 return {"success": False, "message": "one or more missing fields"}, status.HTTP_400_BAD_REQUEST
-            
-            data.update(ssnid=ssnid,patientId=patientId, name=name, age =age, address=address, dateOfJoining=dateOfJoining, roomType=roomType)
+
+            data.update(ssnid=ssnid, patientId=patientId, name=name, age=age,
+                        address=address, dateOfJoining=dateOfJoining, roomType=roomType)
         else:
             return {
-            'success': False,
-            'message': 'Patient not found'            
+                'success': False,
+                'message': 'Patient not found'
             }
-        
+
         return {
             'success': True,
             'message': 'updated'
         }
-        
+
     if(request.method == 'PUT'):
         data = patientModel.Patient.objects(patientId=patientId)
         if(data):
-            data.update(status = 'Discharged')
-            
+            data.update(status='Discharged')
+
             return {
                 'success': True,
                 'message': 'patient discharged'
             }
         else:
             return {
-            'success': False,
-            'message': 'Patient not found'            
+                'success': False,
+                'message': 'Patient not found'
             }
-    
+
     if(request.method == 'DELETE'):
         data = patientModel.Patient.objects(patientId=patientId)
-        
+
         if(data):
             data.delete()
-            
+
             return {
                 'success': True,
                 'message': 'deleted'
             }
         else:
             return {
-            'success': False,
-            'message': 'Patient not found'            
+                'success': False,
+                'message': 'Patient not found'
             }
-        
-        
-        
-    
-
-    
-        
-    
