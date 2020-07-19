03ca7e770a6450bed3fa14a1dc134ad8db8fe331
V. K . Jayendra Varma <keshavvarma.vegiraju@gmail.com>
2020-07-20 00:09:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'kaslhfkasdbviahkvbasivhaknviashflsdajcaiojalkbfiawhwcnaihv'
     MONGODB_SETTINGS = {
         'db': 'hospitalManagement',
-        'host': 'mongodb+srv://casestudyapi:pass123$@tcs-e7psm.mongodb.net/tcsCaseStudy'
+        'host': '***___****'
     }
